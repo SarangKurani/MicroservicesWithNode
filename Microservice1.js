f647aad4c1f21e1cf1a5ab48f99fb075aee4437b
Sarang Kurani <sarang.kurani@gmail.com>
2023-01-17 20:25:48 +0530

    Added logging for whenever a client connects.

--- Microservice1.js
+++ Microservice1.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/microservice1', (req, res) => {
+    console.log("Client connected to microservice1")
     res.send("This is the first microservice.")
 })
 
--- Microservice2.js
+++ Microservice2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/microservice2', (req, res) => {
+    console.log("Client connected to microservice 2")
     res.send("This is the second microservice.")
 })
 
