d1721a07cc0ed0ae5ce688c983d90109bf0c5c41
Sarang Kurani <sarang.kurani@gmail.com>
2023-01-17 22:08:46 +0530

    Added proxy conf, and also functions and html to app template to call microservices.

--- MicroserviceFE/src/app/app.component.ts
+++ MicroserviceFE/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MicroserviceFE';
+ 
+  microserviceResponse: any = "" ;
+
+  constructor(private http: HttpClient) {}
+
+  callMicroservice1() {
+    this.http.get('http://localhost:3000/microservice1').subscribe((response) => {
+      this.microserviceResponse = response;
+    })
+  }
+  
+  callMicroservice2() {
+    this.http.get('http://localhost:3001/microservice2').subscribe((response) => {
+      this.microserviceResponse = response;
+    })
+  }
+
+
 }
