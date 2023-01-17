c7c55d049e48b91d19eedf16b82c0161c6c78455
Sarang Kurani <sarang.kurani@gmail.com>
2023-01-17 22:04:42 +0530

    Updated microservices to use cors.

--- Microservice1.js
+++ Microservice1.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get('/microservice1', (req, res) => {
     console.log("Client connected to microservice1")
-    res.send("This is the first microservice.")
+    res.json("This is the first microservice.")
 })
 
 app.listen(3000, () => {
--- Microservice2.js
+++ Microservice2.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/microservice2', (req, res) => {
     console.log("Client connected to microservice 2")
-    res.send("This is the second microservice.")
+    res.json("This is the second microservice.")
 })
 
 app.listen(3001, () => {
